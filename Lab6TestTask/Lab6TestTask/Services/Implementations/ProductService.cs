f7aa05bc5bd1a6142ff67c36b4b925c385fe54c2
Ligani <bavtrrrel@gmail.com>
2025-10-25 00:08:55 +0300

    test

--- Lab6TestTask/Lab6TestTask/Services/Implementations/ProductService.cs
+++ Lab6TestTask/Lab6TestTask/Services/Implementations/ProductService.cs
@@ -1,6 +1,7 @@
-﻿using Lab6TestTask.Data;
+﻿    using Lab6TestTask.Data;
 using Lab6TestTask.Models;
 using Lab6TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab6TestTask.Services.Implementations;
 
@@ -19,11 +20,22 @@ public class ProductService : IProductService
 
     public async Task<Product> GetProductAsync()
     {
-        throw new NotImplementedException();
+        var product = await _dbContext.Products
+            .Where(p => p.Status == Enums.ProductStatus.Reserved)
+            .OrderByDescending(p => p.Price)
+            .FirstOrDefaultAsync();
+
+        if (product == null)
+            throw new InvalidOperationException("Продукт не найден");
+
+        return product;
     }
 
     public async Task<IEnumerable<Product>> GetProductsAsync()
     {
-        throw new NotImplementedException();
+        var products = await _dbContext.Products
+            .Where(p => p.ReceivedDate.Year == 2025 && p.Quantity > 1000)
+            .ToListAsync();
+        return products;
     }
 }
--- Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
+++ Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
@@ -1,6 +1,7 @@
 ﻿using Lab6TestTask.Data;
 using Lab6TestTask.Models;
 using Lab6TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab6TestTask.Services.Implementations;
 
@@ -19,11 +20,24 @@ public class WarehouseService : IWarehouseService
 
     public async Task<Warehouse> GetWarehouseAsync()
     {
-        throw new NotImplementedException();
+        var warehouse = await _dbContext.Warehouses
+            .OrderByDescending(wh => wh.Products.Sum(p => p.Price))
+            .FirstOrDefaultAsync();
+        if (warehouse == null)
+        {
+            throw new InvalidOperationException("Склад не найден");
+        }
+        return warehouse;
     }
 
     public async Task<IEnumerable<Warehouse>> GetWarehousesAsync()
     {
-       throw new NotImplementedException();
+        var start = new DateTime(2025, 4, 1); 
+        var end = new DateTime(2025, 7, 1);
+
+        var warehouses = await _dbContext.Warehouses
+            .Where(wh => wh.Products.Any(p => p.ReceivedDate >= start && p.ReceivedDate < end))
+            .ToListAsync();
+        return warehouses;
     }
 }
